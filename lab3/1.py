ea21c1278c7eea434b90f1ac7aa838794ecb6fb8
Miller Oleg <milleroleg99@yandex.ru>
2019-12-06 11:54:28 +0700

    Добавлены море небо и земля

---
+++ lab3/1.py
@@ -0,0 +1,16 @@
+from graph import *
+
+windowSize(600, 402)
+canvasSize(600, 402)
+
+brushColor(162,245,255)
+penColor(162,245,255)
+polygon([(0,0),(402,0),(402,124),(0,124)])
+
+brushColor(66,30,224)
+penColor(66,30,224)
+polygon([(0,125),(402,125),(402,184),(0,184),(0,125)])
+
+brushColor(239,246,4)
+penColor(239,246,4)
+polygon([(0,182),(402,182),(402,267),(0,267)])
---
+++ lab3/test.py
@@ -0,0 +1,11 @@
+from graph import *
+
+#brushColor("yellow")
+#rectangle(100,100,200,200)
+point(100,100)
+lineTo(200,100)
+lineTo(200,200)
+lineTo(100,200)
+lineTo(100,100)
+brushColor("yellow")
+polygon
