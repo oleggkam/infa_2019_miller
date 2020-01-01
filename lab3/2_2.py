d8a649bbba5a84057d5431e7ebb3fa384a886cab
Miller Oleg <milleroleg99@yandex.ru>
2020-01-02 00:06:47 +0700

    Была добавлена функция рисования дерева, все работает нормально

--- lab3/2_2.py
+++ lab3/2_2.py
@@ -18,6 +18,12 @@ def paint_white_circle(x,y,r):
     brushColor(254,249,249)
     circle(x,y,r)
 
+def paint_green_circle(x,y,r):
+    '''Используется для создания облака x,y - центр ,r - радиус '''
+    penColor(0,0,0)
+    brushColor(6,82,6)
+    circle(x,y,r)
+
 def paint_cloud(x,y,r):
     '''Рисует облако от точки x,y ,r - радиус одной из окружностей
     чем,меньше радиус, тем меньше облако'''
@@ -28,6 +34,7 @@ def paint_cloud(x,y,r):
     for j in range(0,2):
         paint_white_circle(x-1.75*r,y-r,r)
         x -= 1.5*r
+
 
 def paint_house(x,y,width,height):
     '''x , y - координаты левой верхней точки стен'''
@@ -49,6 +56,27 @@ def paint_roof(x,y,roof_width,roof_height):
     penColor(72,88,93)
     brushColor(236,43,66)
     polygon([(x,y),(x+roof_width/2,y-roof_height/2),(x+roof_width,y),(x,y)])
+
+def paint_tree(x,y,tree_width,tree_height):
+    stvol_height  = tree_height * 0.45
+    stvol_width = stvol_height / 5
+    krona_height  = tree_height * 0.55
+    tree_rad = krona_height / 4
+    paint_stvol(x,y,stvol_width,stvol_height)
+    paint_green_circle(x + 0.5 * stvol_width,y - 3 * tree_rad,tree_rad)
+    paint_green_circle(x - 0.8 * tree_rad,y - 2 * tree_rad,tree_rad)
+    paint_green_circle(x + 0.8 * tree_rad + stvol_width,y - 2 * tree_rad,tree_rad)
+    paint_green_circle(x + 0.5 * stvol_width,y - 1.4 * tree_rad,tree_rad)
+    paint_green_circle(x - 0.6 * tree_rad,y - 0.5 * tree_rad,tree_rad)
+    paint_green_circle(x + 0.6 * tree_rad + stvol_width,y - 0.5 * tree_rad,tree_rad)
+    
+
+def paint_stvol(x,y,st_width,st_height):
+    penColor(0,0,0)
+    brushColor(0,0,0)
+    rectangle(x,y,x+st_width,y+st_height)
+    
+    
     
 paint_cloud(75,42,15)
 paint_cloud(320,53,15)
@@ -56,7 +84,8 @@ paint_cloud(200,60,13)
 
 paint_house(42,140,100,150)
 
-
+paint_tree(287,113,500,105)
+paint_tree(200,200,100,50)
 
 
 run()
